3b53da67ea4ef645e9709a7f54a437da1e6d1dbd
AIcodexgen <ranjith.d@varundigitalmedia.com>
2026-02-07 00:57:56 +0530

    Create cart.php

---
+++ templates/cart.php
@@ -0,0 +1,7 @@
+<div class="gourmetpress-cart">
+    <div id="gp-cart-items"></div>
+    <div class="gp-cart-total">
+        <strong>Total: $<span id="gp-cart-total">0.00</span></strong>
+    </div>
+    <a href="<?php echo home_url('/checkout'); ?>" class="gp-checkout-btn">Proceed to Checkout</a>
+</div>
